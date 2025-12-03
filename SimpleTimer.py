02c33ee7c6510d9aa534ce6f6107ae4def847fc5
detorresyuan <yuandetorres112@gmail.com>
2025-12-03 21:23:21 +0800

    commit just a simple program to start again this hobby

---
+++ SimpleTimer.py
@@ -0,0 +1,14 @@
+import time
+
+myTime = int(input("Enter a desired time in seconds: "))
+
+for x in range(myTime, 0, -1):
+    
+    seconds = x % 60
+    minutes = int(x / 60) 
+    hours = int(x / 3600)
+
+    print(f"{hours:02}:{minutes:02}:{seconds:02}", end = "\r", flush = True)
+    time.sleep(1)
+
+print("Times Up!!!")
