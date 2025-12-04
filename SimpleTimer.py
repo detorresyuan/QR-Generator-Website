9c138347ecbc8214507382028b33c4899260f2f6
detorresyuan <yuandetorres112@gmail.com>
2025-12-04 15:00:27 +0800

    commit changed: Fix typo in minutes calculation in SimpleTimer.py

--- SimpleTimer.py
+++ SimpleTimer.py
@@ -5,7 +5,7 @@ myTime = int(input("Enter a desired time in seconds: "))
 for x in range(myTime, 0, -1):
     
     seconds = x % 60
-    minutes = int(x / 60) 
+    minutes = int(x / 60) % 60
     hours = int(x / 3600)
 
     print(f"{hours:02}:{minutes:02}:{seconds:02}", end = "\r", flush = True)
