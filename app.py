3501b310aa31c7350709f53f1b511ed6d9442ced
detorresyuan <yuandetorres112@gmail.com>
2026-03-01 19:07:24 +0800

    first website yeah

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Group 2 Meme Generator", page_icon=":monkey_face:", layout="wide")
+
+st.subheader("Welcome to the Group 2 Meme Generator! :monkey_face:")
+st.title("Click to monke")
+st.write("This is a group project that utilizes the power of Python using streamlit while also integrating" \
+"log in and database management.")
+st.write("[Learn More>](https://www.youtube.com/watch?v=VqgUkExPvLY&list=WL&index=1&t=217s)")
